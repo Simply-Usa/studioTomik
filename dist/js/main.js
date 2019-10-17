b94ac8dfc9e04e23364ec830f95d2fa44feeafa5
Simply Usa <simplyusa@gmail.com>
2019-10-18 00:41:18 +0600

    uploaded tabs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,50 @@
 $(document).ready(function(){
+	// Клики по табам начало
 	$(function() {
-    var tab = $('#tabs .tabs-items > div'); 
-    tab.hide().filter(':first').show(); 
-    
-    // Клики по вкладкам.
-    $('#tabs .tabs-nav a').click(function(){
-      tab.hide(); 
-      tab.filter(this.hash).slideDown(400); 
-      $('#tabs .tabs-nav a').removeClass('active');
-      $(this).addClass('active');
-      return false;
-    }).filter(':first').click();
-    
-    // Клики по якорным ссылкам.
-    $('.tabs-target').click(function(){
-      $('#tabs .tabs-nav a[href=' + $(this).data('id')+ ']').click();
-    });
-  });
+		var tab = $('#tabs .tabs-items > div'); 
+		tab.hide().filter(':first').show(); 
+		
+		// Клики по вкладкам.
+		$('#tabs .tabs-nav a').click(function(){
+			tab.hide(); 
+			tab.filter(this.hash).slideDown(400); 
+			$('#tabs .tabs-nav a').removeClass('active');
+			$(this).addClass('active');
+			return false;
+		}).filter(':first').click();
+		
+		// Клики по якорным ссылкам.
+		$('.tabs-target').click(function(){
+			$('#tabs .tabs-nav a[href=' + $(this).data('id')+ ']').click();
+		});
+	});
+	// Клики по табам конец
+	
+	
+	// Для фотографий начало
+	$(function() {
+		var tab = $('#tabs .tabs-items-photos > div'); 
+		tab.hide().filter(':first').show(); 
+		
+		// Клики по вкладкам.
+		$('#tabs .tabs-nav-photos a').click(function(){
+			tab.hide(); 
+			tab.filter(this.hash).slideDown(400); 
+			$('#tabs .tabs-nav-photos a').removeClass('active');
+			$(this).addClass('active');
+			return false;
+		}).filter(':first').click();
+		
+		// Клики по якорным ссылкам.
+		$('.tabs-target').click(function(){
+			$('#tabs .tabs-nav-photos a[href=' + $(this).data('id')+ ']').click();
+		});
+	});
+	// Для фотографий конец
+	
+	
+	
+	
 	$(window).scroll(function(){
 		var windowsScroll = $(window).scrollTop();
 		if(windowsScroll >= 100) {
