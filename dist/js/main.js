39b2e7f619f81e6dd5d31c1db94bcaaa49b8914c
Simply Usa <simplyusa@gmail.com>
2019-10-27 13:36:07 +0600

    fixed

--- dist/js/main.js
+++ dist/js/main.js
@@ -43,14 +43,31 @@ $(document).ready(function(){
 	// Для фотографий конец
 	
 	
-	$(window).bind('scroll', function() {
-		var windowTop = $(this).scrollTop();
-		if(windowTop > 500){
-			$('.tabs-items > #tab-2').html('<iframe width="100%" height="315" src="https://www.youtube.com/embed/NU67Bg4Gtck" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/HGh9tjhbQSc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/RP81XnxJ7pc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/54tZnFnPx9Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/UhSNPYDCuag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/zhLBRtkyUAM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-			$(window).unbind('scroll');
-		}
+	$('#videos').click(function(){
+		$('.tabs-items > #tab-2').html('<iframe width="100%" height="315" src="https://www.youtube.com/embed/NU67Bg4Gtck" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/HGh9tjhbQSc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/RP81XnxJ7pc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/54tZnFnPx9Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/UhSNPYDCuag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><iframe width="100%" height="315" src="https://www.youtube.com/embed/zhLBRtkyUAM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 	});
 	
+	// Для прилипающей шапки сайта
+	$(window).scroll(function(){
+		if($(window).scrollTop() >= 3){
+			$('header').css({'padding':'10px','transition':'.3s ease-in-out', 'background': '#161616'})
+			$('.menu li a, .social a i').css({'color': '#fff'});
+			$('.logo').html('<img src="app/img/white-logo.png" alt="Студия">');
+			$('header .btn').addClass('white');
+			// $('header .menu li a').css({'color': '#fff'})
+		}else{
+			$('header').css({'padding':'5px','background': 'none'});
+			$('.menu li a, .social a i').css({'color': '#161616'});
+			$('.logo').html('<img src="app/img/logo.png" alt="Студия">');
+			$('header .btn').removeClass('white');
+		}
+		if($(this).width() < 1200){
+			$('.menu li a').css({'color': '#fff'});
+		}
+		if($(this).width() < 720){
+			$('header .btn').removeClass('white');
+		}
+	});
 	// $(window).scroll(function(){
 	// 	var windowsScroll = $(window).scrollTop();
 	// 	if(windowsScroll >= 100) {
